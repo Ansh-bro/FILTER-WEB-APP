d93036699155c718334765b8005b029f3310f957
Ansh-bro <87030975+Ansh-bro@users.noreply.github.com>
2022-02-17 18:08:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+Nose_X=0;
+Nose_Y=0;
+function preload(){
+  mustache = loadImage('https://i.postimg.cc/TP14nvdX/png-1.png')
+}
+
+function draw(){
+  image(video,0,0,400,300);
+  image(mustache,Nose_X,Nose_Y,80,50)
+}
+
+function setup(){
+  canvas = createCanvas(400,300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(400,300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose',gotPoses);
+}
+
+function takeSnapshot(){
+    save('My_Filter_Image.png');
+}
+
+function modelLoaded(){
+   console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results){
+  if(results.length>0){
+    console.log(results);
+    Nose_X = results[0].pose.nose.x-39;
+    Nose_Y = results[0].pose.nose.y-5;
+    console.log("Nose X="+results[0].pose.nose.x);
+    console.log("Nose Y="+results[0].pose.nose.y);
+  }
+}
